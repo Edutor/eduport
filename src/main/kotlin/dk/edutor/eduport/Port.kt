525ef455d522dfceb7d07cb84922a033c8f6bf79
Anders Kalhauge <anders@kalhauge.dk>
2018-04-17 09:23:26 +0200

    myMavenRepo

--- src/main/kotlin/dk/edutor/eduport/Port.kt
+++ src/main/kotlin/dk/edutor/eduport/Port.kt
@@ -2,5 +2,14 @@ package dk.edutor.eduport
 
 interface Port {
   fun sayHello()
+  fun check(challenge: Challenge, solution: Solution): Assessment
   }
+
+open class PersonIdentifier(val id: Long)
+
+abstract class Solution(val solver: PersonIdentifier, val id: Long)
+
+abstract class Challenge(val portKey: String, val id: Long, val description: String)
+
+abstract class Assessment(val solution: Solution, val grade: Int)
 
