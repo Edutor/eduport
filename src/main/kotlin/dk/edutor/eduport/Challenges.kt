1a6826d2062cefd70a9a4938d2c32191b7acce76
Thomas-Hartmann <thomas@webtrade.dk>
2018-05-01 10:07:45 +0200

    some changes

--- src/main/kotlin/dk/edutor/eduport/Challenges.kt
+++ src/main/kotlin/dk/edutor/eduport/Challenges.kt
@@ -1,18 +1,21 @@
 package dk.edutor.eduport
 
 
-abstract class Challenge(val description: String)
+abstract class Challenge(val description: String, var tags: List<String>)
 
 class StringChallenge(
         val question: String,
-        description: String
-) : Challenge(description)
+        description: String,
+        tags: List<String>
+) : Challenge(description, tags)
 
 
-class CMChallenge(
+class MCChallenge(
+        val id: Int,
 //      answers have a string question and boolean: right question
         val question: String,
         val answers: Map<String,Boolean>,
-        description: String
-) : Challenge(description)
+        description: String,
+        tags: List<String>
+) : Challenge(description, tags)
 
